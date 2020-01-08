0a8d5b8dc965fe0f3579a73f8f2d848900e75e50
Omar Iraqi <o.iraqi@gmail.com>
2020-01-08 14:00:45 +0100

    minor update

--- scripts/ofbiz-immunized.sh
+++ scripts/ofbiz-immunized.sh
@@ -2,4 +2,4 @@ cd ../framework
 ./gradlew agentLibs
 jar cfm ./build/libs/ofbiz-immunizer-agent.jar ../scripts/manifest-ofbiz.mf
 cd ../../ofbiz-framework
-java -javaagent:../immunizer-instrumentation/framework/build/libs/ofbiz-immunizer-agent.jar -Dbuffer=100 -Dskip=5000 -jar build/libs/ofbiz.jar
+java -javaagent:../immunizer-instrumentation/framework/build/libs/ofbiz-immunizer-agent.jar -jar build/libs/ofbiz.jar
