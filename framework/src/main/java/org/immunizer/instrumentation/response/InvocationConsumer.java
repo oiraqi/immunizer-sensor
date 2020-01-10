01625ae560d4b3b8c45b30ee5f17ea55b6a3b0a6
Omar Iraqi <o.iraqi@gmail.com>
2020-01-10 23:50:10 +0100

    added multithreading to agent

--- framework/src/main/java/org/immunizer/instrumentation/response/InvocationConsumer.java
+++ framework/src/main/java/org/immunizer/instrumentation/response/InvocationConsumer.java
@@ -27,7 +27,7 @@ public class InvocationConsumer {
         props.setProperty("auto.commit.interval.ms", "1000");
         props.put("session.timeout.ms", "30000");
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.setProperty("value.deserializer", "org.immunizer.acquisition.InvocationDeserializer");
+        props.setProperty("value.deserializer", "org.immunizer.instrumentaion.response.InvocationDeserializer");
 
         consumer = new KafkaConsumer<String, Invocation>(props);
         Collection<String> topics = Collections.singletonList(TOPIC);        
