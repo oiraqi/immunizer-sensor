4e72dd813ff0f3f77d7f8f282b9f6a9f6079aa67
Omar Iraqi <o.iraqi@gmail.com>
2020-01-12 15:37:24 +0100

    enhancing agent

--- framework/src/main/java/org/immunizer/instrumentation/monitoring/MonitoringAgent.java
+++ framework/src/main/java/org/immunizer/instrumentation/monitoring/MonitoringAgent.java
@@ -151,38 +151,4 @@ public class MonitoringAgent {
 			// Do nothing here
 		}
 	}
-
-	public static class MonitoringAdvice {
-
-		public static InvocationProducer producer = InvocationProducer.getSingleton();
-
-		@Advice.OnMethodEnter
-		public static Invocation onEnter(@Advice.Origin String fullyQualifiedMethodName,
-				@Advice.AllArguments Object[] params) {
-
-			return new Invocation("1.0", fullyQualifiedMethodName, params);
-		}
-
-		@Advice.OnMethodExit(onThrowable = Throwable.class)
-		public static void onExit(@Advice.Enter Invocation invocation,
-				@Advice.Return(typing = Assigner.Typing.DYNAMIC) Object result, @Advice.Thrown Throwable thrown) {
-			
-			if (invocation.returns()) {
-				if (thrown != null) {
-					if (invocation.returnsNumber())
-						invocation.update(Integer.valueOf("0"), true);
-					else
-						invocation.update("NULL", true);
-				} else if (result == null)
-					invocation.update("NULL", false);
-				else
-					invocation.update(result, false);
-			} else if (thrown != null)
-				invocation.update(null, true);
-			else
-				invocation.update(null, false);
-
-			producer.send(invocation);
-		}
-	}
 }
