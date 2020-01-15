4abb91ee5a25a195299d517503c5cc2bf6e86a54
Omar Iraqi <o.iraqi@gmail.com>
2020-01-15 22:51:33 +0100

    small fixes

--- scripts/ofbiz-immunized.sh
+++ scripts/ofbiz-immunized.sh
@@ -1,2 +1,2 @@
 cd ../../ofbiz-framework
-java -javaagent:../immunizer-monitoring/framework/build/libs/immunizer-monitoring-agent.jar -javaagent:../immunizer-response/framework/build/libs/immunizer-response-agent.jar -Dconfig=../immunizer-instrumentation/config/ofbiz-config.json -jar build/libs/ofbiz.jar
+java -javaagent:../immunizer-monitoring/framework/build/libs/immunizer-monitoring-agent.jar -javaagent:../immunizer-response/framework/build/libs/immunizer-response-agent.jar -Dconfig=../immunizer-monitoring/config/ofbiz-config.json -jar build/libs/ofbiz.jar
