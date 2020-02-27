2fefa175716475447c103e5a8308f82eb00754cd
Omar Iraqi <o.iraqi@gmail.com>
2020-02-27 18:06:02 +0100

    update

--- framework/src/main/java/org/immunizer/touchpoint/sensor/InvocationProducer.java
+++ framework/src/main/java/org/immunizer/touchpoint/sensor/InvocationProducer.java
@@ -33,7 +33,7 @@ public class InvocationProducer {
             System.out.println("+++++++++++++++++");
             System.out.println(invocation.getCallStackId());
             System.out.println("+++++++++++++++++");
-            producer.send(new ProducerRecord<String, Invocation>(TOPIC , /*invocation.getCallStackId()*/ 10, "", invocation));
+            producer.send(new ProducerRecord<String, Invocation>(TOPIC , invocation.getCallStackId() % 1000, "", invocation));
             System.out.println("+++++++++++++++++");
         } catch(Throwable th) {
         }
